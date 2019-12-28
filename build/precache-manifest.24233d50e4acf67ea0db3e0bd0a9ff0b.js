814903d2179277b9a68e982370f601b995d8f9b1
nachman <nachman17@gmail.com>
2019-12-28 19:10:58 +0200

    This is a new commit for what I originally planned to be an amendmend

---
+++ build/precache-manifest.24233d50e4acf67ea0db3e0bd0a9ff0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9939db48267c001e2b9a3dbe32404205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2d672c9d82af3ea21508",
+    "url": "/static/css/main.cd504b33.chunk.css"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/static/js/2.c7991b8a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c7991b8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d672c9d82af3ea21508",
+    "url": "/static/js/main.98fe0004.chunk.js"
+  },
+  {
+    "revision": "aa6fed8716c2be098a58",
+    "url": "/static/js/runtime-main.55a556ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
